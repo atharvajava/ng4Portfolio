8a481b6b8385e251139f744d66b59519ae23c9fb
atharvajava <atharvajava@gmail.com>
2017-03-25 02:06:30 +0530

    nav bar

--- myPortfolio/src/app/nav/nav.component.ts
+++ myPortfolio/src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  public check:any=true;
   constructor() { }
 
   ngOnInit() {
   }
+
+  toggle=()=>{
+    $("#wrapper").toggleClass("toggled");
+    this.check=false;
+  };
+  toggleLeave=()=>{
+     $("#wrapper").toggleClass("toggled");
+      $("#wrapper").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",()=>{
+        this.check=true
+      });
+              
+    }
 
 }
