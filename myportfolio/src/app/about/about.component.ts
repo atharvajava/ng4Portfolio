29ffd00b4bbac35546c3080e22566ecc7673bd02
atharvajava <atharvajava@gmail.com>
2017-03-27 01:55:41 +0530

    smooth scroll comp

---
+++ myportfolio/src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { pageTransition } from '../animations';
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
---
+++ myportfolio/src/app/hireme/hireme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hireme',
+  templateUrl: './hireme.component.html',
+  styleUrls: ['./hireme.component.css']
+})
+export class HiremeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
